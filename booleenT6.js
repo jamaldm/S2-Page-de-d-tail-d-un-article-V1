2f96689afc0b443b5f9edfea89de2564f2f8926f
jamaldm <117355905+jamaldm@users.noreply.github.com>
2026-09-21 13:49:01 +0100

    Add files via upload

---
+++ booleenT6.js
@@ -0,0 +1,37 @@
+//exercise 1 
+//test 5 > 2 
+console.log (5>2); // Affiche true
+//test 5 < 2 
+console.log (5<2); // Affiche false
+//test 10 === 10
+console.log (10===10); // Affiche true
+//test 10 !== 10
+console.log (10!==10); // Affiche false
+//test 8 >= 8
+console.log (8>=8); //Affiche true
+//test 3 <= 2 
+console.log (3 <= 2); //Affiche false
+//test true && true
+console.log (true && true); //Affiche true 
+//test true && false 
+console.log (true && false); //Affiche false
+//test false | | true 
+console.log (false || true);//Affiche true
+//test !false 
+console.log (!false); //Affiche true;
+
+//exercise 2
+
+// 1 L’âge est-il supérieur ou égal à 18 ?
+age >= 18;
+// 2 La personne est-elle inscrite ?
+inscrit === true
+// 3 Le paiement est-il effectué ?
+paiement === true
+// 4 L’âge est-il supérieur ou égal à 18 ET la personne est-elle inscrite ?
+age >= 18 && inscrit === true
+// 5 La personne est-elle inscrite OU le paiement est-il effectué ?
+inscrit === true || paiement === true
+
+
+
