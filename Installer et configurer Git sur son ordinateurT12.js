28ab8e2766ebfaff0c31595a44fe1d36ceab01af
jamaldm <117355905+jamaldm@users.noreply.github.com>
2026-09-24 11:07:10 +0100

    Add files via upload

---
+++ Installer et configurer Git sur son ordinateurT12.js
@@ -0,0 +1,15 @@
+🖥️ Vérification de l'installation
+
+$ git --version
+
+git version 2.55.0 ✓ installé
+
+👤 Configuration de l'identité
+
+$ git config --global user.name
+
+jamaldmami ✓ configuré
+
+$ git config --global user.email
+
+jamcolab@gmail.com ✓ configuré
